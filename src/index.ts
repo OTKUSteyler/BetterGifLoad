c7fe81f10a3a094ad31cbbafa91ea2e6e0e42642
OTKUSteyler <qsanom@proton.me>
2026-06-07 14:47:15 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
-
-export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+:3
